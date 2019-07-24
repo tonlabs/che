63bb057e8e50bfba2560c3eeaaf9431e5216bbcd
Denis Kepeschuk <denis.k@tonlabs.io>
2019-07-24 15:45:54 +0300

    Trying to fix view.

--- plugins/plugin-ton-sendmessage/plugin-ton-sendmessage-ide/src/main/java/io/tonlabs/sendmessage/ide/part/SendMessageView.java
+++ plugins/plugin-ton-sendmessage/plugin-ton-sendmessage-ide/src/main/java/io/tonlabs/sendmessage/ide/part/SendMessageView.java
@@ -1,8 +1,10 @@
 package io.tonlabs.sendmessage.ide.part;
 
+import com.google.inject.ImplementedBy;
 import org.eclipse.che.ide.api.mvp.View;
 import org.eclipse.che.ide.api.parts.base.BaseActionDelegate;
 
+@ImplementedBy(SendMessageViewImpl.class)
 public interface SendMessageView extends View<SendMessageView.ActionDelegate> {
   void setVisible(boolean visible);
 
--- plugins/plugin-ton-sendmessage/plugin-ton-sendmessage-ide/src/main/java/io/tonlabs/sendmessage/ide/part/SendMessageViewImpl.java
+++ plugins/plugin-ton-sendmessage/plugin-ton-sendmessage-ide/src/main/java/io/tonlabs/sendmessage/ide/part/SendMessageViewImpl.java
@@ -1,10 +1,13 @@
 package io.tonlabs.sendmessage.ide.part;
 
 import com.google.gwt.user.client.ui.Label;
+import com.google.inject.Inject;
 import org.eclipse.che.ide.api.parts.base.BaseView;
 
 public class SendMessageViewImpl extends BaseView<SendMessageView.ActionDelegate>
     implements SendMessageView {
+
+  @Inject
   public SendMessageViewImpl() {
     Label label = new Label("Hello World");
     this.setContentWidget(label);
