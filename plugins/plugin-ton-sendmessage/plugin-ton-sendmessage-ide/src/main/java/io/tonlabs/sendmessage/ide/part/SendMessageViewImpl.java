9aa9fd02916ce4f900ceb4e27d6cea9306dbf000
Denis Kepeschuk <denis.k@tonlabs.io>
2019-07-27 22:59:34 +0300

    Removed unused import.

--- plugins/plugin-ton-sendmessage/plugin-ton-sendmessage-ide/src/main/java/io/tonlabs/sendmessage/ide/part/SendMessageViewImpl.java
+++ plugins/plugin-ton-sendmessage/plugin-ton-sendmessage-ide/src/main/java/io/tonlabs/sendmessage/ide/part/SendMessageViewImpl.java
@@ -5,7 +5,6 @@ import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
