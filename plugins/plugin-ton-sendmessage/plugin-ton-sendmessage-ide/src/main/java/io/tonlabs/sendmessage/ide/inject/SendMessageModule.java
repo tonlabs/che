754d541eeeb6d1b6e4a1fe4126ff3d56c30863bc
Denis Kepeschuk <denis.k@tonlabs.io>
2019-07-29 22:03:42 +0300

    Removed redundant bindings.

--- plugins/plugin-ton-sendmessage/plugin-ton-sendmessage-ide/src/main/java/io/tonlabs/sendmessage/ide/inject/SendMessageModule.java
+++ plugins/plugin-ton-sendmessage/plugin-ton-sendmessage-ide/src/main/java/io/tonlabs/sendmessage/ide/inject/SendMessageModule.java
@@ -1,14 +1,11 @@
 package io.tonlabs.sendmessage.ide.inject;
 
 import com.google.gwt.inject.client.AbstractGinModule;
-import io.tonlabs.sendmessage.ide.part.SendMessageView;
-import io.tonlabs.sendmessage.ide.part.SendMessageViewImpl;
 import org.eclipse.che.ide.api.extension.ExtensionGinModule;
 
 @ExtensionGinModule
 public class SendMessageModule extends AbstractGinModule {
   @Override
   protected void configure() {
-    this.bind(SendMessageView.class).to(SendMessageViewImpl.class);
   }
 }
