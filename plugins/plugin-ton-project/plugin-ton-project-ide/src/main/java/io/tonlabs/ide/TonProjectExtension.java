39a2c2b150472d7dd79a65983d7ea45ef0136469
Denis Kepeschuk <denis.k@tonlabs.io>
2019-07-30 12:32:44 +0300

    Added actions for help opening.

--- plugins/plugin-ton-project/plugin-ton-project-ide/src/main/java/io/tonlabs/ide/TonProjectExtension.java
+++ plugins/plugin-ton-project/plugin-ton-project-ide/src/main/java/io/tonlabs/ide/TonProjectExtension.java
@@ -14,6 +14,10 @@ package io.tonlabs.ide;
 import static io.tonlabs.shared.Constants.TON_CATEGORY;
 
 import com.google.inject.Inject;
+import io.tonlabs.ide.action.OpenUrlAction;
+import org.eclipse.che.ide.api.action.ActionManager;
+import org.eclipse.che.ide.api.action.DefaultActionGroup;
+import org.eclipse.che.ide.api.action.IdeActions;
 import io.tonlabs.ide.action.AccountStateTvcAction;
 import io.tonlabs.ide.action.SendMessageAction;
 import org.eclipse.che.ide.api.action.ActionManager;
@@ -22,10 +26,19 @@ import org.eclipse.che.ide.api.action.IdeActions;
 import org.eclipse.che.ide.api.extension.Extension;
 import org.eclipse.che.ide.api.icon.Icon;
 import org.eclipse.che.ide.api.icon.IconRegistry;
+import org.vectomatic.dom.svg.ui.SVGResource;
 
 /** TON Project extension that registers actions and icons. */
 @Extension(title = "TON Project Extension", version = "0.0.1")
 public class TonProjectExtension {
+  private final ActionManager actionManager;
+
+  /**
+   * Constructor.
+   *
+   * @param tonProjectResources the resources that contains our icon
+   * @param iconRegistry the {@link IconRegistry} that is used to register our icon
+   */
   @Inject
   public TonProjectExtension(
       TonProjectResources tonProjectResources,
@@ -33,6 +46,7 @@ public class TonProjectExtension {
       ActionManager actionManager,
       SendMessageAction sendMessageAction,
       AccountStateTvcAction accountStateTvcAction) {
+    this.actionManager = actionManager;
     iconRegistry.registerIcon(
         new Icon(TON_CATEGORY + ".samples.category.icon", tonProjectResources.tonIcon()));
 
@@ -44,5 +58,73 @@ public class TonProjectExtension {
 
     actionManager.registerAction("accountStateTvcAction", accountStateTvcAction);
     mainContextMenuGroup.add(accountStateTvcAction);
+  }
+
+  private void registerActions() {
+    DefaultActionGroup helpMenu =
+        (DefaultActionGroup) this.actionManager.getAction(IdeActions.GROUP_HELP);
+
+    this.registerAction(
+        helpMenu, "ideQuickStart", "Che IDE Quick Start", null, "https://ton.dev/quickstart");
+
+    helpMenu.addSeparator();
+
+    this.registerAction(
+        helpMenu,
+        "cCompiler",
+        "LLVM C Compiler Features & Limitations",
+        null,
+        "https://ton.dev/guides?section=c-compiler");
+    this.registerAction(
+        helpMenu,
+        "cCompilerTips",
+        "Online LLVM Compiler Tips",
+        null,
+        "https://ton.dev/guides?section=online-c-compiler");
+    this.registerAction(
+        helpMenu, "tvmCLanguage", "Ñ Language for TVM", null, "https://ton.dev/guides?section=c");
+
+    helpMenu.addSeparator();
+
+    this.registerAction(
+        helpMenu,
+        "solCompiler",
+        "Solidity Compiler Features & Limitations",
+        null,
+        "https://ton.dev/guides?section=solidity-compiler");
+    this.registerAction(
+        helpMenu,
+        "solCompilerTips",
+        "Online Solidity Compiler Tips",
+        null,
+        "https://ton.dev/guides?section=online-solidity-compiler");
+    this.registerAction(
+        helpMenu,
+        "tvmSolLanguage",
+        "Solidity Language for TVM",
+        null,
+        "https://ton.dev/guides?section=solidity");
+
+    helpMenu.addSeparator();
+
+    this.registerAction(
+        helpMenu,
+        "tvmLinker",
+        "Linker Command Line Options",
+        null,
+        "https://ton.dev/guides?section=tvm-linker");
+
+    helpMenu.addSeparator();
+
+    this.registerAction(helpMenu, "tonDev", "Visit TON Dev site", null, "https://ton.dev");
+    this.registerAction(helpMenu, "tonDev", "Visit TON Labs site", null, "https://tonlabs.io");
+  }
+
+  private void registerAction(
+      DefaultActionGroup group, String name, String text, SVGResource svgResource, String url) {
+    OpenUrlAction action = new OpenUrlAction(text, svgResource, url);
+
+    this.actionManager.registerAction(name, action);
+    group.add(action, this.actionManager);
   }
 }
