e13e4e904f95a5124934587c648ad6b5419eb3e9
Denis Kepeschuk <denis.k@tonlabs.io>
2019-08-15 18:11:27 +0300

    Trying to move ton.js into che-ide-gwt-app's public.

--- plugins/plugin-ton-project/plugin-ton-project-ide/webpack.config.js
+++ plugins/plugin-ton-project/plugin-ton-project-ide/webpack.config.js
@@ -2,15 +2,16 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/main/js/ton.js'),
-    output: {
-        path: path.resolve(__dirname, 'src/main/public'),
-        filename: 'ton.js',
-    },
-    plugins: [
-        new webpack.optimize.LimitChunkCountPlugin({
-            maxChunks: 1, // disable creating additional chunks
-        })
-    ],
-    mode: 'development'
+  entry: path.resolve(__dirname, 'src/main/js/ton.js'),
+  output: {
+    path: path.resolve(__dirname,
+        '../../../ide/che-ide-gwt-app/src/main/resources/org/eclipse/che/ide/public'),
+    filename: 'ton.js',
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1, // disable creating additional chunks
+    })
+  ],
+  mode: 'development'
 };
