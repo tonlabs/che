32034d68545e284fe5bd5cd2a97f54f3c75a892d
Denis Kepeschuk <denis.k@tonlabs.io>
2019-08-16 13:01:51 +0300

    Marked java fields/methods as @JsOverlay.

--- plugins/plugin-ton-project/plugin-ton-project-ide/src/main/java/io/tonlabs/ide/sdk/TonSDK.java
+++ plugins/plugin-ton-project/plugin-ton-project-ide/src/main/java/io/tonlabs/ide/sdk/TonSDK.java
@@ -4,15 +4,18 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.DOM;
+import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
 @JsType(isNative = true, namespace = JsPackage.GLOBAL)
 public final class TonSDK {
+  @JsOverlay
   private static TonSDK instance = null;
 
   private TonSDK() {}
 
+  @JsOverlay
   public static TonSDK getInstance() {
     if (instance == null) {
       insertTonJs();
@@ -23,6 +26,7 @@ public final class TonSDK {
     return instance;
   }
 
+  @JsOverlay
   private static void insertTonJs() {
     Element script = DOM.createElement("script");
     script.setAttribute("src", GWT.getModuleBaseForStaticFiles() + "ton.js");
