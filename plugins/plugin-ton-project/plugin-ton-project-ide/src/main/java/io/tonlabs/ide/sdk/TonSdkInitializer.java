dd15ad80041587ccbb6679254e1c05ff12a53b3c
Denis Kepeschuk <denis.k@tonlabs.io>
2019-08-20 17:44:32 +0300

    Reformatted code.

--- plugins/plugin-ton-project/plugin-ton-project-ide/src/main/java/io/tonlabs/ide/sdk/TonSdkInitializer.java
+++ plugins/plugin-ton-project/plugin-ton-project-ide/src/main/java/io/tonlabs/ide/sdk/TonSdkInitializer.java
@@ -10,18 +10,16 @@ public class TonSdkInitializer {
 
   @Inject
   public TonSdkInitializer(/*final RequireJsLoader requireJsLoader*/ ) {
-    ScriptInjector.fromUrl("ton.js")
-        .setWindow(getWindow())
-        .inject();
-//    ScriptInjector.fromString(
-//            "debugger;\n"
-//                + "require(['TonSdk'], function(TonSdk) {\n"
-//                + "\t\tconsole.log(TonSdk);\n"
-//                + "\t\twindow.tonSdk = new TonSdk.TonSdk();\n"
-//                + "\t\twindow.tonSdk.init();\n"
-//                + "\t});")
-//        .setWindow(getWindow())
-//        .inject();
+    ScriptInjector.fromUrl("ton.js").setWindow(getWindow()).inject();
+    //    ScriptInjector.fromString(
+    //            "debugger;\n"
+    //                + "require(['TonSdk'], function(TonSdk) {\n"
+    //                + "\t\tconsole.log(TonSdk);\n"
+    //                + "\t\twindow.tonSdk = new TonSdk.TonSdk();\n"
+    //                + "\t\twindow.tonSdk.init();\n"
+    //                + "\t});")
+    //        .setWindow(getWindow())
+    //        .inject();
     //    requireJsLoader.require(
     //        new Callback<JavaScriptObject[], Throwable>() {
     //          @Override
